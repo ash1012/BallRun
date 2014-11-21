58cfe8b87f1c6e70c17ec017486067688087169b
Ashish <bashish1989@gmail.com>
2014-11-21 22:01:53 +0530

    Initial commit

---
+++ desktop/src/com/gameprac/BallRun/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.gameprac.BallRun.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.gameprac.BallRun.BallRunGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new BallRunGame(), config);
+	}
+}
---
+++ html/src/com/gameprac/BallRun/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.gameprac.BallRun.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.gameprac.BallRun.BallRunGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new BallRunGame();
+        }
+}
