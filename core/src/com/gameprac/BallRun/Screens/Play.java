8683a80b0d065e0cea385380cd23d6f3ecabc1f2
Ashish <bashish1989@gmail.com>
2014-11-22 22:49:15 +0530

    added brick tileset, a ball and a night background

---
+++ core/src/com/gameprac/BallRun/Screens/Play.java
@@ -0,0 +1,65 @@
+package com.gameprac.BallRun.Screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+
+/**
+ * Created by Ashish on 22-11-2014.
+ */
+public class Play implements Screen {
+
+
+    private TiledMap map;
+    private OrthogonalTiledMapRenderer renderer;
+    private OrthographicCamera camera;
+
+    @Override
+    public void show() {
+        map = new TmxMapLoader().load("maps/play.tmx");
+        renderer = new OrthogonalTiledMapRenderer(map);
+        camera = new OrthographicCamera();
+        camera.translate(800/2,480/2);
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(1,1,1,1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        camera.update();
+        renderer.setView(camera);
+        renderer.render();
+    }
+
+    @Override
+    public void hide() {
+        dispose();
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        camera.viewportWidth = width;
+        camera.viewportHeight = height;
+        camera.update();
+    }
+
+    @Override
+    public void dispose() {
+        map.dispose();
+        renderer.dispose();
+    }
+}
--- desktop/src/com/gameprac/BallRun/desktop/DesktopLauncher.java
+++ desktop/src/com/gameprac/BallRun/desktop/DesktopLauncher.java
@@ -7,6 +7,9 @@ import com.gameprac.BallRun.BallRunGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.title="Ball Run v.1.0.0 Alpha";
+        config.width = 800;
+        config.height =480;
 		new LwjglApplication(new BallRunGame(), config);
 	}
 }
